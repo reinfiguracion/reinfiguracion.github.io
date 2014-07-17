84afde5465ee501d2563f410b2970583d0d153dd
Reina Figuracion <reinafiguracion@gmail.com>
2014-07-17 23:13:30 +0800

    Squip

---
+++ squip/squip/index.php
@@ -0,0 +1,159 @@
+<?php include "common/header.php"; ?>
+
+
+<section id="intro">
+	<div class="container">
+		<img src="images/squip-marketing-logo.png" alt="" class="img-responsive">
+		<div class="page-header">
+			<h1>Let Your Website Do the Selling</h1>
+			<p> Everything you need to make your website 1000x more effective in getting customers and clients. We blend <span class="highlight">design</span>, <span class="highlight">copy</span> and <span class="highlight">online marketing</span> into a potent solution.</p>
+		</div>
+		<a href="#services" class="btn btn-embossed btn-success btn-lg scroll">See services</a>
+	</div> <!-- /container -->
+</section>
+
+<section id="services">
+	<div class="container">
+		<div class="page-header">
+			<h2>Every Little Piece Makes Up the Big Picture of a Successful Online Business</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, rerum, nemo, sapiente, veniam voluptate obcaecati.</p>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<ul class="media-list">
+					<li class="media">
+						<img class="pull-right media-object" src="images/landing-page-ico.jpg">
+						<div class="media-body">
+							<h4 class="media-heading">Landing Pages</h4>
+							<p>A landing page is where transactions are initiated. We make design + copy that will effectively guide, inspire and prompt site visitors to sign up & buy your products and services. </p>
+						</div>
+					</li>
+
+					<li class="media">
+						<img class="pull-right media-object" src="images/content-ico.jpg">
+						<div class="media-body">
+							<h4 class="media-heading">Content</h4>
+							<p>Become a trusted info-giver to gain influence over your niche or present your products & services effectively to get people to want it through content that works. We are content strategists that can create a mixed-channel content marketing strategy for you.</p>
+						</div>
+					</li>
+
+				</ul>
+
+			</div>
+			<div class="col-lg-6">
+				<ul class="media-list">
+					<li class="media">
+						<img class="pull-right media-object" src="images/seo-ico.jpg">
+						<div class="media-body">
+							<h4 class="media-heading">SEO</h4>
+							<p>Search is how your customers find you online. We mix & match the right SEO solutions to make sure you get found by the right people with intentions to buy what you're offering.</p>
+						</div>
+					</li>
+
+					<li class="media">
+						<img class="pull-right media-object" src="images/email-social-ico.jpg">
+						<div class="media-body">
+							<h4 class="media-heading">Emails & Social</h4>
+							<p>Email and social media marketing are all about getting intimate with your market. We provide top-notch email marketing and social media services for you to get up close and personal with potential buyers and induce sales.</p>
+						</div>
+					</li>
+
+				</ul>
+			</div>
+		</div>
+
+	</div> <!-- /container -->
+</section>
+
+<section id="process">
+	<div class="container">
+		<div class="page-header">
+			<h2>Our Process Leaves No Stone Unturned </h2>
+			<p>We have a very thorough process to tightly zoom in on what's essential. This is the only way we can determine how we can better work with you to bring you the success you aim for!</p>
+		</div>
+		<div class="timeline">
+			<div class="time-header">
+				<h3>Onboarding</h3>
+				<p>Sitting in a virtual coffee shop where we meet and greet each other. This is the perfect time to inquire about our services + <span class="highlight">free consultation!</span> </p>
+			</div>
+			<ul>
+				<li class="step-01">
+					<h4>1. Gather Data</h4>
+					<p>Fill out a client profile questionnaire so we know more about your business. We'll base our work from data you've given us and other research we might do for our part.</p>
+				</li>
+				
+				<li class="step-02">
+					<h4>2. Ideas</h4>
+					<p>We lock ourselves in a room, go to the drawing board and sink into mental state to prepare for the next stage -- the actual work.</p>
+				</li>
+
+
+				<li class="step-03">
+					<h4>3. Design & Build </h4>
+					<p>Work on your order within the timeline agreed upon.</p>
+				</li>
+
+
+				<li class="step-04">
+					<h4>4. Design Iteration & Approval</h4>
+					<p>We receive feedback from your team and make necessary changes.</p>
+				</li>
+
+			</ul>
+			<div class="time-header">
+				<h3>Delivery</h3>
+				<p>Deliver your order or implement the service you ordered. This is the final stage. </p>
+			</div>
+		</div>
+		<p class="help-block">Please note: Stages may differ depending on your needs and the service you ordered which will be set during the initial onboarding.</p>
+	</div>
+</section>
+
+<section id="quote">
+	<div class="container">
+		<div class="page-header">
+			<h2>Get a Quote</h2>
+			<p>Due to high demand we can only accept 3 projects per month right now. If you’re serious about getting your website selling, move fast to get your project in, or get in the waiting line.</p>
+		</div>
+		<form action="" class="quote-form">
+			<div class="form-group">
+				<label class="sr-only">What is your full name?</label>
+				<input type="text" class="form-control input-lg" placeholder="What is your full name?">
+			</div>
+
+			<div class="form-group">
+				<label class="sr-only">What is your email address?</label>
+				<input type="text" class="form-control input-lg" placeholder="What is your email address?">
+			</div>
+
+			<div class="form-group">
+				<label class="sr-only">What is your skype name?</label>
+				<input type="text" class="form-control input-lg" placeholder="What is your skype name?">
+			</div>
+
+			<div class="form-group">
+				<select class="form-control input-lg">
+					<option selected disabled>How can we help you?</option>
+					<option>Landing Pges</option>
+					<option>SEO</option>
+					<option>Content</option>
+					<option>Emails & Social</option>
+					<option>Haven't Decided</option>
+				</select>
+			</div>
+			<button class="btn btn-lg btn-danger btn-embossed btn-block" type="submit">Get a Quote</button>
+			<p class="help-block">By clicking Get Final Quote, you agree to our Terms & Conditions.</p>
+		</form>
+
+		
+	</div>
+</section>
+
+<section id="contact">
+	<div class="container">
+		<div class="page-header">
+			<p>Copyright &copy; 2013 Squip Marketing | Questions? E-mail cheby@squipmarketing.com or Skype cheby.squipmarketing</p>
+		</div>
+	</div>
+</section>
+<?php include "common/footer.php"; ?>
