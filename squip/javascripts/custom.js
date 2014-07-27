df563efcf9e85fb84a00f4a066506f7588496fb8
Reina Figuracion <reinafiguracion@gmail.com>
2014-07-27 19:37:31 +0800

    Signed-off-by: Reina Figuracion <reinafiguracion@gmail.com>

---
+++ squip/javascripts/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".type").typed({
+    strings: ["Startup","Ecommerce", "Blog"],
+    typeSpeed: 200,
+    backDelay:5000,
+    loop: true,
+    loopCount: false,
+  });
+});
